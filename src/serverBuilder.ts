5e64addd1e463e031b1cd60ad3939f767c241b45
melancholiai <57397441+melancholiai@users.noreply.github.com>
2021-02-09 15:05:13 +0200

    Initial commit

---
+++ src/serverBuilder.ts
@@ -0,0 +1,55 @@
+import express, { Router } from 'express';
+import bodyParser from 'body-parser';
+import { OpenapiViewerRouter, OpenapiRouterConfig } from '@map-colonies/openapi-express-viewer';
+import { getErrorHandlerMiddleware } from '@map-colonies/error-express-handler';
+import { middleware as OpenApiMiddleware } from 'express-openapi-validator';
+import { container, inject, injectable } from 'tsyringe';
+import { RequestLogger } from './common/middlewares/RequestLogger';
+import { Services } from './common/constants';
+import { IConfig, ILogger } from './common/interfaces';
+import { resourceNameRouterFactory } from './resourceName/routes/resourceNameRouter';
+@injectable()
+export class ServerBuilder {
+  private readonly serverInstance: express.Application;
+
+  public constructor(
+    @inject(Services.CONFIG) private readonly config: IConfig,
+    private readonly requestLogger: RequestLogger,
+    @inject(Services.LOGGER) private readonly logger: ILogger
+  ) {
+    this.serverInstance = express();
+  }
+
+  public build(): express.Application {
+    this.registerPreRoutesMiddleware();
+    this.buildRoutes();
+    this.registerPostRoutesMiddleware();
+
+    return this.serverInstance;
+  }
+
+  private buildDocsRoutes(): void {
+    const openapiRouter = new OpenapiViewerRouter(this.config.get<OpenapiRouterConfig>('openapiConfig'));
+    openapiRouter.setup();
+    this.serverInstance.use(this.config.get<string>('openapiConfig.basePath'), openapiRouter.getRouter());
+  }
+
+  private buildRoutes(): void {
+    this.serverInstance.use('/resourceName', resourceNameRouterFactory(container));
+    this.buildDocsRoutes();
+  }
+
+  private registerPreRoutesMiddleware(): void {
+    this.serverInstance.use(bodyParser.json());
+
+    const ignorePathRegex = new RegExp(`^${this.config.get<string>('openapiConfig.basePath')}/.*`, 'i');
+    const apiSpecPath = this.config.get<string>('openapiConfig.filePath');
+    this.serverInstance.use(OpenApiMiddleware({ apiSpec: apiSpecPath, validateRequests: true, ignorePaths: ignorePathRegex }));
+
+    this.serverInstance.use(this.requestLogger.getLoggerMiddleware());
+  }
+
+  private registerPostRoutesMiddleware(): void {
+    this.serverInstance.use(getErrorHandlerMiddleware((message) => this.logger.log('error', message)));
+  }
+}
