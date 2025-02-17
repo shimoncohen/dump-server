743dcc04cb35465046ae01597f0a810a70f180a1
razbroc <razbroc@gmail.com>
2025-02-17 17:20:07 +0200

    feat: update packege.json and serverBuilder.ts to boilerplate

--- src/serverBuilder.ts
+++ src/serverBuilder.ts
@@ -3,22 +3,25 @@ import bodyParser from 'body-parser';
 import compression from 'compression';
 import { Logger } from '@map-colonies/js-logger';
 import httpLogger from '@map-colonies/express-access-log-middleware';
-import { defaultMetricsMiddleware } from '@map-colonies/telemetry';
-import { OpenapiViewerRouter, OpenapiRouterConfig } from '@map-colonies/openapi-express-viewer';
+import { OpenapiViewerRouter } from '@map-colonies/openapi-express-viewer';
 import { getErrorHandlerMiddleware } from '@map-colonies/error-express-handler';
 import { middleware as OpenApiMiddleware } from 'express-openapi-validator';
+import { getTraceContexHeaderMiddleware } from '@map-colonies/telemetry';
 import { inject, injectable } from 'tsyringe';
+import { collectMetricsExpressMiddleware } from '@map-colonies/telemetry/prom-metrics';
+import { Registry } from 'prom-client';
 import { SERVICES } from './common/constants';
-import { IConfig } from './common/interfaces';
 import { DUMP_METADATA_ROUTER_SYMBOL } from './dumpMetadata/routes/dumpMetadataRouter';
+import { ConfigType } from './common/config';
 
 @injectable()
 export class ServerBuilder {
   private readonly serverInstance: express.Application;
 
   public constructor(
-    @inject(SERVICES.CONFIG) private readonly config: IConfig,
+    @inject(SERVICES.CONFIG) private readonly config: ConfigType,
     @inject(SERVICES.LOGGER) private readonly logger: Logger,
+    @inject(SERVICES.METRICS) private readonly metricsRegistry: Registry,
     @inject(DUMP_METADATA_ROUTER_SYMBOL) private readonly dumpsRouter: express.Router
   ) {
     this.serverInstance = express();
@@ -33,9 +36,12 @@ export class ServerBuilder {
   }
 
   private buildDocsRoutes(): void {
-    const openapiRouter = new OpenapiViewerRouter(this.config.get<OpenapiRouterConfig>('openapiConfig'));
+    const openapiRouter = new OpenapiViewerRouter({
+      ...this.config.get('openapiConfig'),
+      filePathOrSpec: this.config.get('openapiConfig.filePath'),
+    });
     openapiRouter.setup();
-    this.serverInstance.use(this.config.get<string>('openapiConfig.basePath'), openapiRouter.getRouter());
+    this.serverInstance.use(this.config.get('openapiConfig.basePath'), openapiRouter.getRouter());
   }
 
   private buildRoutes(): void {
@@ -44,15 +50,18 @@ export class ServerBuilder {
   }
 
   private registerPreRoutesMiddleware(): void {
-    this.serverInstance.use('/metrics', defaultMetricsMiddleware());
-    this.serverInstance.use(httpLogger({ logger: this.logger }));
-    if (this.config.get<boolean>('server.response.compression.enabled')) {
-      this.serverInstance.use(compression(this.config.get<compression.CompressionFilter>('server.response.compression.options')));
-    }
-    this.serverInstance.use(express.json(this.config.get<bodyParser.Options>('server.request.payload')));
+    this.serverInstance.use(collectMetricsExpressMiddleware({ registry: this.metricsRegistry }));
+    this.serverInstance.use(httpLogger({ logger: this.logger, ignorePaths: ['/metrics'] }));
 
-    const ignorePathRegex = new RegExp(`^${this.config.get<string>('openapiConfig.basePath')}/.*`, 'i');
-    const apiSpecPath = this.config.get<string>('openapiConfig.filePath');
+    if (this.config.get('server.response.compression.enabled')) {
+      this.serverInstance.use(compression(this.config.get('server.response.compression.options') as unknown as compression.CompressionFilter));
+    }
+
+    this.serverInstance.use(bodyParser.json(this.config.get('server.request.payload')));
+    this.serverInstance.use(getTraceContexHeaderMiddleware());
+
+    const ignorePathRegex = new RegExp(`^${this.config.get('openapiConfig.basePath')}/.*`, 'i');
+    const apiSpecPath = this.config.get('openapiConfig.filePath');
     this.serverInstance.use(OpenApiMiddleware({ apiSpec: apiSpecPath, validateRequests: true, ignorePaths: ignorePathRegex }));
   }
 
