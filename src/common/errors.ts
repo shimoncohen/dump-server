12919a3a554922e3892f2f4c844b0048626bdc42
Niv Greenstein <greenstein9@gmail.com>
2025-02-24 14:44:17 +0200

    refactor: added TimeoutError. implemented HttpError interface

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -1,3 +1,14 @@
-export class NotFoundError extends Error {}
+import { HttpError } from '@map-colonies/error-express-handler';
+import httpStatus from 'http-status-codes';
 
-export class DumpNameAlreadyExistsError extends Error {}
+export class NotFoundError extends Error implements HttpError {
+  public readonly status = httpStatus.NOT_FOUND;
+}
+
+export class DumpNameAlreadyExistsError extends Error implements HttpError {
+  public readonly status = httpStatus.CONFLICT;
+}
+
+export class TimeoutError extends Error implements HttpError {
+  public readonly status = httpStatus.REQUEST_TIMEOUT;
+}
