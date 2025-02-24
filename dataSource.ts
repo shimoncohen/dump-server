daec99cae5f19715a328afd8631510b9e524ceca
Niv Greenstein <greenstein9@gmail.com>
2025-02-24 14:50:17 +0200

    fix: updated createDataSourceOptions

--- dataSource.ts
+++ dataSource.ts
@@ -1,13 +1,13 @@
 import { DataSource } from 'typeorm';
 import { getConfig, initConfig } from './src/common/config';
-import { createConnectionOptions } from './src/common/db';
+import { createDataSourceOptions } from './src/common/db';
 
 const dataSourceFactory = async (): Promise<DataSource> => {
   await initConfig(true);
   const config = getConfig();
   const connectionOptions = config.get('db');
   const appDataSource = new DataSource({
-    ...createConnectionOptions(connectionOptions),
+    ...createDataSourceOptions(connectionOptions),
     entities: ['src/entity/models/*.ts'],
     migrationsTableName: 'custom_migration_table',
     migrations: ['db/migrations/*.ts'],
