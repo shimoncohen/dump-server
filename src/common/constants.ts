7125bae48198edd4a24c1969bd665e6915f52379
Niv Greenstein <greenstein9@gmail.com>
2025-02-24 14:44:55 +0200

    refactor: changed SERVICE to be Record<string, symbol>

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -7,14 +7,19 @@ export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/, /^.*\/metrics.*/];
 
-export enum SERVICES {
-  LOGGER = 'ILogger',
-  CONFIG = 'IConfig',
-  TRACER = 'TRACER',
-  METRICS = 'Metrics',
-  HEALTHCHECK = 'HealthCheck',
-  OBJECT_STORAGE = 'IObjectStorage',
-}
+/* eslint-disable @typescript-eslint/naming-convention */
+export const SERVICES = {
+  LOGGER: Symbol('ILogger'),
+  CONFIG: Symbol('IConfig'),
+  TRACER: Symbol('TRACER'),
+  METRICS: Symbol('Metrics'),
+  HEALTHCHECK: Symbol('HealthCheck'),
+  OBJECT_STORAGE: Symbol('IObjectStorage'),
+  CLEANUP_REGISTRY: Symbol('CLEANUP_REGISTRY'),
+} satisfies Record<string, symbol>;
+/* eslint-enable @typescript-eslint/naming-convention */
+
+export const ON_SIGNAL = Symbol('onSignal');
 
 export const DB_TIMEOUT = 5000;
 
